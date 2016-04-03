56f8d38f51cf53fa6cd97c739e965d1e0f584515
Michael Spear <badcab@localhost.localdomain>
2016-04-03 13:14:17 -0500

    init commit

---
+++ app/config/setup.sql
@@ -0,0 +1,15 @@
+CREATE TABLE songs (
+	Id int(11) primary key,
+	song_path varchar(256),
+	artist varchar(256),
+	title varchar(256),
+	votes int(4), --0 means not in heat yet
+	category varchar(32)
+);
+
+CREATE TABLE heat
+	Id int(11) primary key,
+	song_ids varchar(48) --(a json string of song id)
+);
+
+--there is no need for a queue just pull the last thing off the heat

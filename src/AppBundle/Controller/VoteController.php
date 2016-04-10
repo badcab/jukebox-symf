5804f20eeb34a2e0b1d4969e4327f26b78c26ab3
Michael Spear <github@bsd4breakfast.us>
2016-04-10 08:14:27 -0500

    more setup

--- src/AppBundle/Controller/VoteController.php
+++ src/AppBundle/Controller/VoteController.php
@@ -10,28 +10,28 @@ use Symfony\Component\HttpFoundation\Request;
 class VoteController extends Controller
 {
     /**
-     * @param Request $request
      * @Route ("/", name="homepage")
+     * @Route ("/vote", name="homepage")
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction(Request $request) //request not used but will be for other items
+    public function indexAction() //request not used but will be for other items
     {
-        //should pass in current vote heat to display list
+        //should pass in current vote heat to display list, this should be stored using a cookie
         return $this->render('vote/index.html.twig');
     }
 
     /**
-     * @param Request $request
-     * @Route ("/Cast", name="CastVote")
+     * @param int $id
+     * @Route ("/vote/cast/{id}", name="CastVote")
      */
-    public function castAction(Request $request){
+    public function castAction($id){
         //this is an ajax endpoint
         //I don think I need a twig file for this and the return is of type json
     }
 
     /**
      * @param Request $request
-     * @Route ("/getCount", name="getCount")
+     * @Route ("vote/getCount", name="getCount")
      */
     public function getCountAction(Request $request){
         //this should return json and be called every second or something, will update current vote count on a heat
